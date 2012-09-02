e0313760bef42d63f298bbdfb073a1af9c84ddb9
Benton Roberts <benton@bentonroberts.com>
2012-09-01 21:01:51 -0400

    restructure includes

--- lib/rds_backup_service.rb
+++ lib/rds_backup_service.rb
@@ -1,3 +1,7 @@
 %w{rubygems bundler}.each {|lib| require lib}
 Bundler.setup
-require File.join(File.dirname(__FILE__), "rds_backup_service/rds_backup_service")
+
+# Load the other files in this library
+%w{ version rds_backup_service config model/backup_job }.each do |file|
+  require File.join(File.dirname(__FILE__), "rds_backup_service/#{file}")
+end
