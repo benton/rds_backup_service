21679f248d95d663ab0225d95bb6af1cc5b837cd
Benton Roberts <benton@bentonroberts.com>
2012-09-03 17:33:48 -0400

    add basic email model

--- lib/rds_backup_service.rb
+++ lib/rds_backup_service.rb
@@ -3,6 +3,6 @@ Bundler.setup
 
 # Load all the other files in this library, except the service
 %w{ version rds_backup_service config
-    model/backup_job model/delayed_job }.each do |file|
+    model/backup_job model/delayed_job model/email }.each do |file|
   require File.join(File.dirname(__FILE__), "rds_backup_service/#{file}")
 end
---
+++ lib/rds_backup_service/model/email.rb
@@ -0,0 +1,37 @@
+require 'mail'
+module RDSBackup
+  # an email representation of a Job, that can send itself to recipients.
+  class Email
+
+    attr_reader :job
+
+    def initialize(backup_job)
+      @job = backup_job
+    end
+
+    def send!
+      raise "job #{job.backup_id} has no email option" unless job.options['email']
+      main_text = body_text # define local variables for closure over Mail.new
+      recipients, header = job.options['email'], "Backup of RDS #{job.rds_id}"
+      mail = Mail.new do
+        from    'rdsbackupservice@mdsol.com'
+        to      recipients
+        subject header
+        body    "#{main_text}\n"
+      end
+      mail.deliver!
+    end
+
+    def body_text
+      msg = "Hello.\n\n"
+      if job.status == 200
+        msg += "Your backup of database #{job.rds_id} is complete.\n"+
+          (job.files.empty? ? "" : "Output is at #{job.files.first.url}\n")
+      else
+        msg += "Your backup is incomplete: #{job.status}\n"
+      end
+      msg += "Job status is at #{job.status_url}"
+    end
+
+  end
+end
